299cea6a3b9809256bcd9ee77ae32a9d8e3142c4
Sabrina Pereira <sabrina.pereira@students.olin.edu>
2018-11-06 12:32:02 -0500

    Adding IR sensor and Potenttiometer testing code

--- SensorsTest/SensorsTest.ino
+++ SensorsTest/SensorsTest.ino
@@ -21,31 +21,40 @@
 */
 
 // These constants won't change. They're used to give names to the pins used:
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogInPin1 = A1;
+const int photocellPin = A0;  // Analog input pins that the potentiometers are attached to
+const int irPin = A1;
+const int potPin = A2;
 const int analogOutPin = 9; // Analog output pin that the LED is attached to
 
 int sensorValue = 0;        // value read from the pot
 int sensorValue1 = 0;
+int sensorValue2 = 0;
 int outputValue = 0;        // value output to the PWM (analog out)
+
+int roomLight = 0;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
+  roomLight = analogRead(photocellPin);
 }
 
 void loop() {
+
+Serial.println(roomLight);
+  
   // read the analog in value:
-  sensorValue = analogRead(analogInPin);
-  sensorValue1 = analogRead(analogInPin1);
+  sensorValue = analogRead(photocellPin);
+  sensorValue1 = analogRead(irPin);
+  sensorValue2 = analogRead(potPin);
   // map it to the range of the analog out:
 
-  if(sensorValue < 100)
+  if (sensorValue < 100)
   {
     outputValue = 0;
   }
 
-  else if(sensorValue < 375)
+  else if (sensorValue < 375)
   {
     outputValue = map(sensorValue, 0, 1023, 100, 200);
   }
@@ -54,17 +63,19 @@ void loop() {
   {
     outputValue = 255;
   }
-  
+
   // change the analog out value:
   analogWrite(analogOutPin, outputValue);
 
   // print the results to the Serial Monitor:
-  Serial.print("Light sensor = ");
+  Serial.print("Light = ");
   Serial.print(sensorValue);
-  Serial.print("\t Distance sensor = ");
-  Serial.println(sensorValue1);
+  Serial.print("  Distance = ");
+  Serial.print(sensorValue1);
+  Serial.print("  Pot = ");
+  Serial.println(sensorValue2);
 
   // wait 2 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
-  delay(2);
+  delay(30);
 }
