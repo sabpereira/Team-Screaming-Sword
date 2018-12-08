085b1a4401aac26a98c1fed099bfcc3c69292aba
Phoebe <pamory@wellesley.edu>
2018-12-08 17:13:13 -0500

    flashes two leds - can be used for fairy circle

--- LightInteractions/LightInteractions.ino
+++ LightInteractions/LightInteractions.ino
@@ -1,7 +1,8 @@
-int photocellValues[] = {0, 0, 0, 0, 0, 0};        // values read from the photocells
+//int photocellValues[6];// = {0, 0, 0, 0, 0, 0};        // values read from the photocells
 int roomLight = 0;
 int lightThresholdOffset = 50;
 int lightCounter = 0;
+unsigned long flashCounter = 0;
 
 const int ul = A0; // Upper left photocell
 //Upper platform only has 3 mushrooms and 3 static plants, doesn't need a photocell
@@ -11,20 +12,57 @@ const int ll = A3; // Lower left photocell
 const int lo = A4; // Lower photocell
 const int lr = A5; // Lower right photocell
 
-const int 
+const int fc1 = 12; //fairy circle digital pin - does not need to be a PWM pin
+const int fc2 = 11; //second fairy circle digital pin - does not need to be a PWM pin
+
+unsigned long previousMillis = 0;
+unsigned long currentMillis = millis();
+int interval = 500; // time interval between blinks
+
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ul, INPUT);
-  pinMode(ur, INPUT);
-  pinMode(l, INPUT);
-  pinMode(ll, INPUT);
-  pinMode(lo, INPUT);
-  pinMode(lr, INPUT);
-  roomLight = (analogRead(ul)+analogRead(ur)+analogRead(l)+analogRead(ll)+analogRead(lo)+analogRead(lr))/6;
+  
+  pinMode(fc1, OUTPUT);
+  pinMode(fc2, OUTPUT);
+  
+  //Calibrate to room lighting
+  // roomLight = (analogRead(ul)+analogRead(ur)+analogRead(l)+analogRead(ll)+analogRead(lo)+analogRead(lr))/6;
+  roomLight = analogRead(ul);
 }
 
 void loop() {
-  photocellValues[] = {analogRead(ul), analogRead(ur), analogRead(l), analogRead(ll), analogRead(lo), analogRead(lr)};
+  //photocellValues = {analogRead(ul), analogRead(ur), analogRead(l), analogRead(ll), analogRead(lo), analogRead(lr)};
+
+  //Fairy circle: Lower left. Controlled by one photocell, involves two digital pins. 
+  // If you get close to it, every other mushroom should flash. Each four mushrooms are connected in series
+  // and controlled by one digital pin.
+
+  currentMillis = millis();
+
+  // if the fairy circle photocell is below a certain interval, start the flashing routine
+  if (analogRead(ul) <= (roomLight - lightThresholdOffset/2)) {
+    if ((currentMillis - previousMillis) > interval) {
+    
+      //save the last time you blinked the LED
+      previousMillis = currentMillis;
+      flashCounter++; //increment counter
+
+      //switches which LEDs are on
+      if (flashCounter % 2 == 1) {
+        digitalWrite(fc1, HIGH);
+        digitalWrite(fc2, LOW);
+      }
+      if (flashCounter % 2 == 0){
+        digitalWrite(fc1, LOW);
+        digitalWrite(fc2, HIGH);
+      }
+    
+    }
+  }
+  else {
+    digitalWrite(fc1, LOW);
+    digitalWrite(fc2, LOW);
+  }
   
 }
